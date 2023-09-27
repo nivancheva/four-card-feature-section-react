2b20e345f2e71e31f3dbcf0acd04d46bc5663d93
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-09-27 09:42:04 +0300

    add and style title

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-        Hello Nelly!
-    </div>
+    <main className='wraper'>
+      <div className='container'>
+        <div className='title'>
+          <h1>Reliable, efficient delivery<span>Powered by Technology</span></h1>
+          <p>Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+        </div>
+        
+      </div>
+    </main>
   );
 }
 
