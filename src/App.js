afebdc5f3ed498ec5b89c0ed9b4db212b1907f49
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-09-27 10:17:58 +0300

    add cards info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import { useState } from 'react';
+import dataJson from './data.json';
+import Cards from './Cards';
 
 function App() {
+  const [cards] = useState(dataJson)
+
   return (
     <main className='wraper'>
       <div className='container'>
@@ -8,6 +13,13 @@ function App() {
           <h1>Reliable, efficient delivery<span>Powered by Technology</span></h1>
           <p>Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
         </div>
+
+        <div className='card container'></div>
+          {cards.map((card, idx) => {
+            return (
+              <Cards key={idx} {...card}/>
+            )
+          })}
         
       </div>
     </main>
